e6967a1d271b9f2071572a118a9b2777a5a4b3c9
Jagadeesh <jagad2sh@gmail.com>
2026-03-30 00:23:19 +0530

    player and match validation added

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,11 @@
 class Match < ApplicationRecord
   belongs_to :winner, class_name: "Player"
   belongs_to :loser, class_name: "Player"
+
+  validate :check_duplicate_player
+
+  private
+  def check_duplicate_player
+    errors.add(:base, "A player cannot play against themselves") if self.winner_id == self.loser_id
+  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,7 @@
 class Player < ApplicationRecord
-  validates :name, uniqueness: true
-
   has_many :wins, class_name: "Match", foreign_key: "winner_id"
   has_many :losses, class_name: "Match", foreign_key: "loser_id"
+
+  validates :name, presence: true, uniqueness: true
+  before_save { self.name = name.downcase }
 end
